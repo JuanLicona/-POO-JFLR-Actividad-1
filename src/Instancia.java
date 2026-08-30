27b54aae75143d9cc9059de86e33123064b2950e
Juan Licona <juanfliconaromero@gmail.com>
2026-08-30 16:52:01 -0500

    Creacion de Constructores por defecto y parametrizados

---
+++ src/Instancia.java
@@ -0,0 +1,9 @@
+public class Instancia {
+
+    static void main(String[] args) {
+
+        Libro L1 = new Libro();
+        Libro L2 = new Libro("El desastroso Max Crumbly. Caos escolar", "Rachel Renée Russell", 240, "14/02/2017");
+
+    }
+}
