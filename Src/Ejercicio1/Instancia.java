4d69fbfb59b7b12631064ec3d925724a64ffa4c9
Juan Licona <juanfliconaromero@gmail.com>
2026-08-31 04:04:46 -0500

    Cambios y Arreglos de Carpetas

--- Src/Ejercicio1/Instancia.java
+++ Src/Ejercicio1/Instancia.java
@@ -1,3 +1,5 @@
+package Ejercicio1;
+
 public class Instancia {
 
     static void main(String[] args) {
@@ -8,7 +10,7 @@ public class Instancia {
 
         //Gett y Impresión
 
-        System.out.println("Libro Escogido");
+        System.out.println("ee.Libro Escogido");
         System.out.println("Titulo:" + L1.getTitulo());
         System.out.println("Autor:" + L1.getAutor());
         System.out.println("Numero de Paginas:" + L1.getNumeroPaginas());
