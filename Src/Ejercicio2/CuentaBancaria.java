012235cdafad6d520d3e8fa1eae919f6bba31fd3
Juan Licona <juanfliconaromero@gmail.com>
2026-09-07 09:22:36 -0500

    Ejercicio 2 y Creacion de Constructor con 2 parametros

---
+++ Src/Ejercicio2/CuentaBancaria.java
@@ -0,0 +1,24 @@
+package Ejercicio2;
+
+public class CuentaBancaria {
+
+    int numeroCuenta;
+    int saldo;
+    int TipoCuenta;
+
+
+    public CuentaBancaria(){
+
+        int numeroCuenta = 0;
+        int saldo = 0;
+        int TipoCuenta = 0;
+    }
+
+    public CuentaBancaria(int numeroCuenta,int TipoCuenta){
+
+        this.numeroCuenta = numeroCuenta;
+        this.TipoCuenta = TipoCuenta;
+
+    }
+
+}
